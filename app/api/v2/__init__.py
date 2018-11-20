a10eb245f802806102ea99929f9b053a1c1ac83b
queenfiona <fionaitya@gmail.com>
2018-11-20 23:34:40 +0300

    [Chore

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,1 +1,7 @@
 """Docstring for v2's  __init__.py."""
+from flask import Blueprint
+from flask_restful import Api
+
+version_2 = Blueprint('apiv2', __name__)
+
+api = Api(version_2, prefix="/api/v2")
