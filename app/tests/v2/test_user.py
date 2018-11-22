b5bf245e5d570e8965fe64982a984b3ae24fc960
queenfiona <fionaitya@gmail.com>
2018-11-23 01:52:10 +0300

    [Feature #161968318] test to determine if the username is an empty string

--- app/tests/v2/test_user.py
+++ app/tests/v2/test_user.py
@@ -27,3 +27,19 @@ class TestLoginCase(BaseCase):
         result = json.loads(res2.data)
         self.assertEqual(result["message"], "user already exists")
         self.assertEqual(res2.status_code, 409)
+
+    def test_valid_username_input(self):
+        """Docstring for test_valid_username method."""
+        self.data = {
+            "email": "fionaityang@gmail.com",
+            "first_name": "Fiona",
+            "last_name": "Murie",
+            "password": "quifi",
+            "username": ""
+        }
+        res = self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.data),
+            content_type='application/json')
+        result = json.loads(res.data)
+        self.assertEqual(result['message'], "Please enter a valid username")
+        self.assertEqual(res.status_code, 400)
