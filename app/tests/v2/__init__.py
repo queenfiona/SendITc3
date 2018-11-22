7a4c9669e1fc9c33668ab65e9e5a2e18fc7a0f25
queenfiona <fionaitya@gmail.com>
2018-11-23 02:31:14 +0300

    [Feature #161968339] add dummy data to test login

--- app/tests/v2/__init__.py
+++ app/tests/v2/__init__.py
@@ -1,4 +1,5 @@
 """Docstring for v2 __init__.py."""
+import json
 import unittest
 from app import create_app
 from app.db_config import destroy_tables
@@ -25,6 +26,60 @@ class BaseCase(unittest.TestCase):
             "last_name": "Murie",
             "password": "quifi",
             "username": "quifi"
+        }
+        self.signup_admin = {
+            "email": "fionaityang@gmail.com",
+            "first_name": "Mary",
+            "last_name": "Doe",
+            "password": "mary",
+            "username": "mary",
+            "role": "admin"
+        }
+        self.login_data = {
+            "username": "quifi",
+            "password": "quifi"
+        }
+        self.login_admin = {
+            "username": "mary",
+            "password": "mary"
+        }
+        self.parcel_data = {
+            "item_shipped": "Books",
+            "origin": "Nairobi",
+            "destination": "Kisumu",
+            "current_location": "Nairobi",
+            "pickup_location": "Kisumu",
+            "weight": 12
+        }
+        self.specific_data = {
+            "item_shipped": "Books",
+            "origin": "Nairobi",
+            "destination": "Kisumu",
+            "current_location": "Nairobi",
+            "pickup_location": "Kisumu",
+            "weight": 12
+        }
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.signup_data),
+            content_type='application/json')
+        res = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.login_data),
+            content_type='application/json')
+        data = json.loads(res.get_data(as_text=True))
+        self.token = data["access_token"]
+        self.user_headers = {
+            'AUTHORIZATION': 'Bearer ' + self.token
+        }
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.signup_admin),
+            content_type='application/json')
+        res = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.login_admin),
+            content_type='application/json')
+        data = json.loads(res.get_data(as_text=True))
+        self.token = data["access_token"]
+        self.admin_headers = {
+            'AUTHORIZATION': 'Bearer ' + self.token
         }
 
     def tearDown(self):
