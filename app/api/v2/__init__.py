461a09074a67bcd36237634e611ecc00f4a678f4
queenfiona <fionaitya@gmail.com>
2018-11-23 00:01:39 +0300

    [Feature #161968318] create a base class for tests in v2's init file

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,21 @@
 """Docstring for v2's  __init__.py."""
 from flask import Blueprint
 from flask_restful import Api
+from .views.parcel_views import (
+    ParcelOrderView, UserOrderView, AllOrdersView, DestinationView, StatusView,
+    PresentLocView)
+from .views.user_views import UserRegistration, UserLogin
 
 version_2 = Blueprint('apiv2', __name__)
 
 api = Api(version_2, prefix="/api/v2")
+api.add_resource(ParcelOrderView, "/parcels", strict_slashes=False)
+api.add_resource(UserOrderView, "/parcels/<string:username>", strict_slashes=False)
+api.add_resource(AllOrdersView, "/parcels")
+api.add_resource(DestinationView, "/parcels/<int:parcel_id>/destination")
+api.add_resource(StatusView, "/parcels/<int:parcel_id>/status")
+api.add_resource(PresentLocView,
+                 "/parcels/<int:parcel_id>/presentLocation")
+api.add_resource(UserRegistration, "/auth/signup")
+api.add_resource(UserLogin, "/auth/login")
+# api.add_resource(RefreshTokenView, "/auth/refresh")
