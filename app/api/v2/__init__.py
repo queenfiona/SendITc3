1ea06d079c23bacc0d0072ba9f049e4324d73239
queenfiona <fionaitya@gmail.com>
2018-11-23 00:46:09 +0300

    [Feature #161968318] Remove the special characters from file

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -4,25 +4,18 @@ from flask_restful import Api
 from .views.parcel_views import (
     ParcelOrderView, UserOrderView, AllOrdersView, DestinationView, StatusView,
     PresentLocView)
-<<<<<<< HEAD
 from .views.user_views import UserRegistration
-=======
-from .views.user_views import UserRegistration, UserLogin
->>>>>>> e0ed436485e8680c88595a48cec7edde56c0f8b0
+
 
 version_2 = Blueprint('apiv2', __name__)
 
 api = Api(version_2, prefix="/api/v2")
 api.add_resource(ParcelOrderView, "/parcels", strict_slashes=False)
-api.add_resource(UserOrderView, "/parcels/<string:username>", strict_slashes=False)
+api.add_resource(UserOrderView, "/parcels/<string:username>",
+                 strict_slashes=False)
 api.add_resource(AllOrdersView, "/parcels")
 api.add_resource(DestinationView, "/parcels/<int:parcel_id>/destination")
 api.add_resource(StatusView, "/parcels/<int:parcel_id>/status")
 api.add_resource(PresentLocView,
                  "/parcels/<int:parcel_id>/presentLocation")
 api.add_resource(UserRegistration, "/auth/signup")
-<<<<<<< HEAD
-=======
-api.add_resource(UserLogin, "/auth/login")
-# api.add_resource(RefreshTokenView, "/auth/refresh")
->>>>>>> e0ed436485e8680c88595a48cec7edde56c0f8b0
