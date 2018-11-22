8ad3f821bd059efe3c33f468e33ce1234e1e0a41
queenfiona <fionaitya@gmail.com>
2018-11-23 02:33:52 +0300

    [Feature #161968339] create models and views for signin

--- app/api/v2/views/user_views.py
+++ app/api/v2/views/user_views.py
@@ -9,7 +9,7 @@ class UserRegistration(Resource, UserModel):
     """User registration class."""
 
     def post(self):
-        """Post method."""
+        """Registration post method."""
         parser = reqparse.RequestParser()
         parser.add_argument('first_name', type=str,
                             help="First name is missing", required=True)
@@ -46,3 +46,24 @@ class UserRegistration(Resource, UserModel):
                 "message": "user already exists"}), 409)
 
         return make_response(jsonify(payload), 201)
+
+
+class UserLogin(Resource, UserModel):
+    """User login class."""
+
+    def post(self):
+        """Login post method."""
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str,
+                            help="Username is missing", required=True)
+        parser.add_argument("password", type=str,
+                            help="Password is missing", required=True)
+        user_data = parser.parse_args()
+        username = user_data['username']
+        if not CheckUserInput().check_if_input_is_string(username):
+            return {"message": "Please enter a valid username"}, 400
+        password = user_data['password']
+        if not CheckUserInput().check_if_input_is_string(password):
+            return {"message": "Please enter a valid password"}, 400
+        payload = UserModel().user_signin(username=username, pwd=password)
+        return make_response(jsonify(payload), 200)
