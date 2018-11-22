159f72d3a786913ec7f69dc64d52fc14db141824
queenfiona <fionaitya@gmail.com>
2018-11-23 02:29:41 +0300

    [Feature #161968339]add route for login in v2's init file

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,10 +1,11 @@
 """Docstring for v2's  __init__.py."""
 from flask import Blueprint
 from flask_restful import Api
-from .views.user_views import UserRegistration
+from .views.user_views import UserRegistration, UserLogin
 
 
 version_2 = Blueprint('apiv2', __name__)
 
 api = Api(version_2, prefix="/api/v2")
 api.add_resource(UserRegistration, "/auth/signup")
+api.add_resource(UserLogin, "/auth/login")
