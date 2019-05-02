1288314c90437e5fb42824ee62303016dab79ede
queenfiona <fionaitya@gmail.com>
2019-05-02 15:51:16 +0300

    delete travis.sh file

--- .travis.sh
+++
@@ -1,7 +0,0 @@
-export FLASK_APP="run.py"
-export FLASK_ENV="testing"
-export FLASK_DEBUG=True
-export JWT_SECRET_KEY="fiona"
-export DATABASE_DEVELOP="host='localhost' port='5432' dbname='sendit' user='app' password='app'"
-export DATABASE_TEST="host='localhost' port='5432' dbname='sendit_test' user='postgres' password=''"
-export DATABASE_PRODUCTION='postgres://lzkeoyabateebv:2eeda542ecd71f311f82018d4528f7e7c4f6118444b5847b80097e79333c67b9@ec2-54-204-36-249.compute-1.amazonaws.com:5432/d7be81aif8bjb0'
